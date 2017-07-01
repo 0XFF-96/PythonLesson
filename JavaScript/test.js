acd61abe32d3e2f62384071ad29344df2ce48980
Alex1996 <13420134410@163.com>
2017-07-01 10:15:56 +0800

    JavaScript lesson2

---
+++ JavaScript/test.js
@@ -0,0 +1,30 @@
+var test;
+
+var test_2 = "shiyanlou";
+
+x = 100;
+y = "hello"
+z = True;
+
+cars=["Audi", "BMW", "Volvo"];
+
+var carname="shiyanlou";
+var carname="shiyanlou";
+var answer="I Love shiyanlou"
+var answer=' I Love shiyanlou'
+
+var x1=34.00;
+var x2=34;
+
+var x=true
+var y=false
+
+var boys=new Array();
+boys[0]="Tom"
+boys[1]="Jack"
+boys[2]="Alex"
+
+var boys=new Array("Tom", "Jack" , "Alex");
+var boys=["Tom", "Jack", "Alex"];
+
+
