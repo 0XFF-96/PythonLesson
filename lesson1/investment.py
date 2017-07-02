f37f4259aec28b01e39b75426a9a7fbe1bb4e5a1
Alex1996 <13420134410@163.com>
2017-07-02 23:35:31 +0800

    second lesson2 review python

---
+++ lesson1/investment.py
@@ -0,0 +1,16 @@
+#1/usr/bin/env python3
+
+amount = float(input("Enter amount: "))
+inrate = float(input("Enter Interest rate: "))
+
+period = int(input("Enter period: "))
+
+value = 0
+year = 1
+
+while year <= period:
+	value = amount + (inrate * amount)
+	print("year {} Rs. {:.2f}".format(year,value))
+	amount = value
+	year = year +1
+
---
+++ lesson1/testhundred.py
@@ -0,0 +1,8 @@
+#1/usr/bin/env python3
+
+number = int(input("Enter an integer:"))
+if number <100:
+	print("Your number is smaller than 100")
+else:
+	print("Your number is greater than 100")
+
