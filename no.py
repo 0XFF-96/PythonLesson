1d02f49a109e04c53f286fcf6ca8a769f55ad6a9
Alex1996 <13420134410@163.com>
2017-07-06 16:12:30 +0800

    just add a memo

---
+++ no.py
@@ -0,0 +1,8 @@
+#hi , sorry bro
+# I ca't code without Good connectiong to The Internet!
+# This place is suck! 
+# You gotta to remeber this situation!
+# I'm very very upset about this ! 
+# I am going to coffeshop
+# can't you just check ou
+
